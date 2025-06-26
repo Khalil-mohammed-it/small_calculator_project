4c8f5ffe0f71bd759943d38ed7d65d58ea322962
Khalil Mohammed <Khalil.it@gmx.de>
2025-06-27 00:02:30 +0200

    Funktion mult und main

---
+++ multiplikation.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int mult(int a, int b) {
+  if (a == 0 || b == 0) {
+    return 0;
+  }
+
+  else if (b > 0) {
+    return a + mult(a, b - 1);
+  }
+
+  else {
+    return -a + mult(a, b + 1);
+  }
+}
+
+int main() {
+  int a, b;
+  printf("Eingabe: ");
+  scanf("%d %d", &a, &b);
+
+  int result = mult(a, b);
+  printf("Ausgabe: %d\n", result);
+  return 0;
+}
